e3446bb2893ccf17bfe6f4a50b90193bd318fa97
Diego Sepulveda <diego@rflex.cl>
2020-06-22 13:52:38 -0400

    Primera maquetacion

---
+++ src/components/CardCliente.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card class="mx-auto" max-width="344" outlined>
+      <v-row>
+          <v-col>
+                <v-chip
+                    class="ma-2"
+                    color="orange"
+                    text-color="white"
+                >
+                Premium
+                <v-icon right>mdi-star</v-icon>
+                </v-chip>
+          </v-col>
+          <v-col>
+              <v-chip
+                    class="ma-2"
+                    color="orange"
+                    text-color="white"
+                >
+                Premium
+                <v-icon right>mdi-star</v-icon>
+                </v-chip>
+          </v-col>
+          <v-col>
+              <v-chip
+                    class="ma-2"
+                    color="orange"
+                    text-color="white"
+                >
+                Premium
+                <v-icon right>mdi-star</v-icon>
+                </v-chip>
+          </v-col>
+          <v-col>
+              <v-chip
+                    class="ma-2"
+                    color="orange"
+                    text-color="white"
+                >
+                Premium
+                <v-icon right>mdi-star</v-icon>
+                </v-chip>
+          </v-col>
+      </v-row>
+
+    <v-card-actions>
+      <v-btn text>Detalles</v-btn>
+      <v-btn text>Button</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "CardCliente",
+
+  data: () => ({})
+};
+</script>
---
+++ src/views/Inicio.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+      >
+        <v-row>
+          <v-col>
+              <card-cliente></card-cliente>
+          </v-col>
+          <v-col>
+              <card-cliente></card-cliente>
+          </v-col>
+          <v-col>
+              <card-cliente></card-cliente>
+          </v-col>
+          <v-col>
+              <card-cliente></card-cliente>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import CardCliente from '@/components/CardCliente.vue'
+
+
+export default {
+    props: {
+        source: String
+    },
+    components : {
+        CardCliente
+    },
+    data: () => ({drawer: null}),
+}
+</script>
