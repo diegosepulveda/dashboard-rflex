af8cdd38d116037a1b3815a83436182b65073b27
Diego Sepulveda <diego@rflex.cl>
2020-07-02 12:16:09 -0400

    Implementa graficos

---
+++ src/components/Bar.vue
@@ -0,0 +1,36 @@
+<script>
+import { Bar } from 'vue-chartjs'
+import _ from 'lodash'
+
+export default {
+  extends: Bar,
+  props : ['listaData','tituloGrafico'],
+  data : function(){
+    return {
+
+
+      data: {
+            labels: _.map(this.listaData,'fecha'),
+            datasets: [{
+                label: this.tituloGrafico,
+				data: _.map(this.listaData,'cuenta'),
+				backgroundColor: '#f87979',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        },
+    }
+  },
+  mounted () {
+    this.renderChart(this.data, this.options)
+  }
+}
+</script>
--- src/components/CardCliente.vue
+++ src/components/CardCliente.vue
@@ -69,13 +69,13 @@
             <v-spacer></v-spacer>
           </v-card-title>
           <v-card-text>
-                <v-data-table
+                <!-- <v-data-table
                     :headers="headersListaErrores"
                     :items="listaErroresPorFecha"
                     :items-per-page="20"
                     class="elevation-1"
-                ></v-data-table>
-
+                ></v-data-table> -->
+               <bar :lista-data="listaErroresPorFecha" titulo-grafico="Lista Errores"></bar>
           </v-card-text>
           <v-card-actions>
             <v-btn
@@ -98,8 +98,13 @@
 </template>
 
 <script>
+import Bar from "@/components/Bar.vue";
+
 export default {
     name: "CardCliente",
+    components: {
+        Bar
+    },
     props : {
         'cliente' : Object
     },
@@ -142,20 +147,7 @@ export default {
         headersListaErrores: [
           { text: 'Fecha', value: 'fecha' },
           { text: 'Cuenta', value: 'cuenta' },
-        ],
-        items: [
-          { tab: 'One', content: 'Tab 1 Content' },
-          { tab: 'Two', content: 'Tab 2 Content' },
-          { tab: 'Three', content: 'Tab 3 Content' },
-          { tab: 'Four', content: 'Tab 4 Content' },
-          { tab: 'Five', content: 'Tab 5 Content' },
-          { tab: 'Six', content: 'Tab 6 Content' },
-          { tab: 'Seven', content: 'Tab 7 Content' },
-          { tab: 'Eight', content: 'Tab 8 Content' },
-          { tab: 'Nine', content: 'Tab 9 Content' },
-          { tab: 'Ten', content: 'Tab 10 Content' },
-        ],
-
+        ]
     })
 };
 </script>
