a6419cedd5b488cc0590967e8f9f61163d39fc83
diego <diegoti89@gmail.com>
2020-10-20 06:49:46 -0300

    Agregea la posibilidad de https en servidor

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const fs = require('fs')
+
 module.exports = {
+
+  devServer: {
+    // https: {
+    //   key: fs.readFileSync('./localhost.key'),
+    //   cert: fs.readFileSync('./localhost.crt'),
+    // },
+    // public: 'https://localhost:8080/'
+  },
+
   "transpileDependencies": [
     "vuetify"
   ]
